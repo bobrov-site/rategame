3260dbc9225a13ea0bca0ab499e320095d9fa1f2
Daniil Bobrov <allodrmen@yandex.ru>
2024-07-28 18:00:01 +0300

    сreated routes

---
+++ src/app/routes.ts
@@ -0,0 +1,27 @@
+export const Routes: {
+    [key: string] : {
+      path: string,
+      title: string,
+    }
+  } = {
+    home: {
+      path: '/',
+      title: 'Главная',
+    },
+    about: {
+      path: '/about',
+      title: 'О сервисе',
+    },
+    trends: {
+      path: '/trends',
+      title: 'Популярное',
+    },
+    terms: {
+      path: '/terms',
+      title: 'Условия',
+    },
+    privacy: {
+      path: '/privacy',
+      title: 'Политика конфиденциальности',
+    },
+  };
