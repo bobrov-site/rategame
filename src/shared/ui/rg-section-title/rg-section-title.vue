8b954f41415292d68fdaba82941934de8bed483f
Daniil Bobrov <allodrmen@yandex.ru>
2024-07-28 18:52:39 +0300

    created rg-section-title

---
+++ src/shared/ui/rg-section-title/rg-section-title.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2 class="text-4xl font-bold">
+    {{ props.title }}
+  </h2>
+</template>
+
+<script setup lang="ts">
+import type { SectionTitle } from '~/shared/types/sectionTitle';
+
+const props = withDefaults(defineProps<SectionTitle>(), {
+  title: 'Заголовок секции',
+});
+</script>
+
+<style scoped>
+
+</style>
