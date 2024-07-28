713c78b33434527090963540bc8b1fc5028aa13b
Daniil Bobrov <allodrmen@yandex.ru>
2024-07-28 17:09:11 +0300

    set nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
+import { paths } from './node.const';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ...paths,
+  buildDir: './.nuxt',
+  serverDir: './server',
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
 })
