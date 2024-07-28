a79775e3496fd0349df97a1dc653087e99c5fe15
Daniil Bobrov <allodrmen@yandex.ru>
2024-07-28 17:28:59 +0300

    change layout

--- src/widgets/layouts/default.vue
+++ src/widgets/layouts/default.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="layout-wrapper relative">
-      <main>
-        <slot />
-      </main>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
- 
-  </script>
-  
+  <div class="rg-default">
+    <rr-header />
+    <main class="container mx-auto">
+      <slot />
+    </main>
+    <rr-footer />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
