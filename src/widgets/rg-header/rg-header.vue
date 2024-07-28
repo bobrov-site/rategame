16562c2fb2fadb259169c8b039c49e27bfedb20b
Daniil Bobrov <allodrmen@yandex.ru>
2024-07-28 18:15:37 +0300

    created header

---
+++ src/widgets/rg-header/rg-header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="rg-header pt-3 pb-3">
+    <div class="container mx-auto flex justify-between items-center">
+      <div class="rg-logo">
+        <NuxtLink class="font-bold text-xl flex items-center" to="/">
+          RateGame
+          <UIcon name="i-mdi-robot-love-outline ms-1" />
+        </NuxtLink>
+        <!-- mdi:robot-love-outline -->
+      </div>
+      <UHorizontalNavigation :links="links" />
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { Routes } from '~/app/routes';
+
+const links = [
+  {
+    label: Routes.trends.title,
+    to: Routes.trends.path,
+  },
+  {
+    label: Routes.registration.title,
+    to: Routes.registration.path,
+  },
+  {
+    label: Routes.login.title,
+    to: Routes.login.path,
+  },
+];
+</script>
+
+<style scoped>
+
+</style>
