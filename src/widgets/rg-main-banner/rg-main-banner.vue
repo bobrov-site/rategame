ce78dec3a3e6690c565e86bfc0ce088d3d3ac59c
Daniil Bobrov <allodrmen@yandex.ru>
2024-07-28 22:38:21 +0300

    added styles

--- src/widgets/rg-main-banner/rg-main-banner.vue
+++ src/widgets/rg-main-banner/rg-main-banner.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="hover:shadow-md bg-primary
+    class="hover:shadow-md bg-primary rg-main-banner
   w-full p-4 rounded-2xl flex flex-col justify-center items-center"
   >
     <h1 class="font-bold text-center text-6xl text-white mb-4">
       Лучший трекер видеоигр
     </h1>
-    <p class="text-white max-w-xl mb-4 text-center w-full text-center">
+    <p class="text-white max-w-xl mb-4 text-center w-full">
       RateGame - сервис позволяющий следить за новыми
       видеоиграми, оценивать их рейтинг и получать рекомендации на основе их
       просмотров.
@@ -22,9 +22,7 @@
 </template>
 
 <script setup lang="ts">
-
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
 </style>
