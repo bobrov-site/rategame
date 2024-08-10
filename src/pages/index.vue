663c3e9b688cec944e5666eaf6a94b93a3312aec
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-10 22:49:29 +0300

    fetch data

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,23 @@
 </template>
 
 <script setup lang="ts">
+import type { IgdbAuth } from '~/entities/igdb/domen/igdbAuth';
+import { useIgdbStore } from '~/entities/igdb/model/igdb-store';
 
+onMounted(async() => {
+  await fetchToken();
+  await fetchGames();
+});
+const igdbStore = useIgdbStore();
+const fetchToken = async() => {
+  const data : IgdbAuth = await $fetch('/igdb/token', { method: 'POST' });
+  igdbStore.accessToken = data.access_token;
+};
+
+const fetchGames = async() => {
+  const data = await $fetch('/igdb/games', { method: 'GET', params: { accessToken: igdbStore.accessToken } });
+  console.log(data);
+};
 </script>
 
 <style scoped>
