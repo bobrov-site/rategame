758a9ce2953369ae33ee19cd5b1f485fb6807098
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-10 22:49:16 +0300

    get token

--- server/routes/igdb/token.post.ts
+++ server/routes/igdb/token.post.ts
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 export default defineEventHandler(async (event) => {
   const { igdbIdClient, igdbSecretClient } = useRuntimeConfig(event);
-  const response = await axios.post('https://id.twitch.tv/oauth2/token', {
-    client_id: igdbIdClient,
-    client_secret: igdbSecretClient,
-    grant_type: 'client_credentials',
-  });
-  return response.data;
+  try {
+    const response = await axios.post('https://id.twitch.tv/oauth2/token', {
+      client_id: igdbIdClient,
+      client_secret: igdbSecretClient,
+      grant_type: 'client_credentials',
+    });
+    return response.data;
+  } catch (e) {
+    Promise.reject(e);
+  }
 });
--- server/utils/igdb/axiosInstanceIgdb.ts
+++ server/utils/igdb/axiosInstanceIgdb.ts
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 const axiosInstanceIgdb = axios.create({
   baseURL: 'https://api.igdb.com/v4',
-  headers: {
-    Authorization: `Bearer ${process.env.IGDB_TOKEN}`,
-  },
 });
 
 export default axiosInstanceIgdb;
