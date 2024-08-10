7d0f91028d046c9930c4112c5b89734d86edc6d2
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-10 21:49:27 +0300

    added theme switcher

---
+++ src/features/rg-theme-switch/rg-theme-switch.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="rg-theme-switch">
+    <UButton
+      color="primary"
+      variant="ghost"
+      icon="mdi:theme-light-dark"
+      @click="isDark = !isDark"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode();
+const isDark = computed({
+  get () {
+    return colorMode.value === 'dark';
+  },
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+  },
+});
+</script>
+
+<style scoped>
+
+</style>
