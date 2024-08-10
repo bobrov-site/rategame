45fc94c2408198501a631adc4a1f25a893c3ef10
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-10 21:51:29 +0300

    renamed axios instance

--- server/utils/igdb/axiosInstanceIgdb.ts
+++ server/utils/igdb/axiosInstanceIgdb.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const axiosInstanceAvito = axios.create({
+const axiosInstanceIgdb = axios.create({
   baseURL: 'https://api.igdb.com/v4',
   headers: {
     Authorization: `Bearer ${process.env.IGDB_TOKEN}`,
   },
 });
 
-export default axiosInstanceAvito;
+export default axiosInstanceIgdb;
