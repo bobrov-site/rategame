51a1bcbf83150f3b4105d0ac8f5c6eeb9cf2a635
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-11 21:56:57 +0300

    remove fetch games from main

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -11,17 +11,11 @@ import { useIgdbStore } from '~/entities/igdb/model/igdb-store';
 
 onMounted(async() => {
   await fetchToken();
-  await fetchGames();
 });
 const igdbStore = useIgdbStore();
 const fetchToken = async() => {
   const data : IgdbAuth = await $fetch('/igdb/token', { method: 'POST' });
   igdbStore.accessToken = data.access_token;
-};
-
-const fetchGames = async() => {
-  const data = await $fetch('/igdb/games', { method: 'GET', params: { accessToken: igdbStore.accessToken } });
-  console.log(data);
 };
 </script>
 
